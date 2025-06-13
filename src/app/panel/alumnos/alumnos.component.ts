7d26bbe28c4a0eae24ac39f72c7f9737968685b1
STRATOS <stratos@iMac-de-STRATOS-2.local>
2025-06-13 01:36:02 -0600

    cambios 13 jun

--- src/app/panel/alumnos/alumnos.component.ts
+++ src/app/panel/alumnos/alumnos.component.ts
@@ -3,6 +3,10 @@ import { FormBuilder, Validators} from '@angular/forms';
 import { UsuariosService } from '../../services/usuarios.service';
 import { User } from '../../interfaces/User';
 import { appsettings } from '../../settings/appsettings';
+import DataTable from 'datatables.net-bs4';
+
+
+import  * as functRS  from '../../../assets/js/funcionesrs';
 
 @Component({
   selector: 'app-alumnos',
@@ -16,6 +20,7 @@ export class AlumnosComponent {
   public usuarios: User[] = []
   public user: Array<any> = []
   public baseUrl: string = appsettings.urlImg;
+  resultado:any
 
   editForm=this.formBuilder.group({
     id:[''],
@@ -27,7 +32,7 @@ export class AlumnosComponent {
     telefono:['',Validators.required],
     genero:['',Validators.required],
     fecha_alta:['',Validators.required],
-    empresa:['',Validators.required],
+    empresa:[''],
   })
 
   constructor(private formBuilder:FormBuilder) { 
@@ -37,29 +42,29 @@ export class AlumnosComponent {
   cargar_table(){
 
     document.addEventListener("DOMContentLoaded", () => {
-      
+
     });
- 
+
   }
 
   listar(){
     this.usuariosService.listaUsers().subscribe({
-       next: (data) =>{
-        
+     next: (data) =>{
+
         //console.log(data['value'])
         //console.log(data.value.length)
-        if (data.value.length > 0) {
-          this.usuarios = data['value']
-        }
-      }, error:(error) =>{
-        //console.log(error.message); 
+      if (data.value.length > 0) {
+        this.usuarios = data['value']
       }
-    })
+    }, error:(error) =>{
+        //console.log(error.message); 
+    }
+  })
   }
 
   getUser(id:number){
     this.usuariosService.getUsers(id).subscribe({
-       next: (user) =>{
+      next: (user) =>{
         console.log(user)
         this.editForm.controls.id.setValue(user.value.id)
         this.editForm.controls.nombre.setValue(user.value.nombre)
@@ -70,7 +75,7 @@ export class AlumnosComponent {
         this.editForm.controls.genero.setValue(user.value.genero.toString())
         this.editForm.controls.fecha_alta.setValue(user.value.fecha_alta)
         this.editForm.controls.empresa.setValue(user.value.empresa)
-        
+
       }, error:(error) =>{
         console.log(error.message); 
       }
@@ -80,10 +85,79 @@ export class AlumnosComponent {
   editarU(id:number){
     this.getUser(id)
   }
+
+  actualizar(){
+    if(this.editForm.valid){
+      this.usuariosService.editUser(this.editForm.value).subscribe({
+        next: (user) =>{
+          console.log(user)
+          this.editForm.controls.id.setValue(user.value.id)
+          this.editForm.controls.nombre.setValue(user.value.nombre)
+          this.editForm.controls.apellido_p.setValue(user.value.apellido_p)
+          this.editForm.controls.apellido_m.setValue(user.value.apellido_m)
+          this.editForm.controls.email.setValue(user.value.email)
+          this.editForm.controls.telefono.setValue(user.value.telefono)
+          this.editForm.controls.genero.setValue(user.value.genero.toString())
+          this.editForm.controls.fecha_alta.setValue(user.value.fecha_alta)
+          this.editForm.controls.empresa.setValue(user.value.empresa)
+
+        }, error:(error) =>{
+          console.log(error.message); 
+        },
+        complete: () => {
+          console.info("Update completo");
+          //this.router.navigateByUrl('/panel');
+          //window.location.href="/panel";
+          location.reload()
+          
+          
+        }
+      })
+    }else{
+      this.editForm.markAllAsTouched();
+    }
+  }
+
+  
+  get nombre()
+  {
+    return this.editForm.controls.nombre;
+  }
+  get apellido_p()
+  {
+    return this.editForm.controls.apellido_p;
+  }
+  get apellido_m()
+  {
+    return this.editForm.controls.apellido_m;
+  }
+  get email()
+  {
+    return this.editForm.controls.email;
+  }
+  get telefono()
+  {
+    return this.editForm.controls.telefono;
+  }
+  get password()
+  {
+    return this.editForm.controls.password;
+  }
+  get genero()
+  {
+    return this.editForm.controls.genero;
+  }
+  get fecha_alta()
+  {
+    return this.editForm.controls.fecha_alta;
+  }
   
 
   ngOnInit(): void {
     //this.cargar_table()
     this.listar()
+    console.log(functRS.userdatatable())
   }
+
+
 }
