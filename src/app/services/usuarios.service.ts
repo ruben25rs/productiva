fae0928da3796492721aaabc081a54c7daedb67f
STRATOS <stratos@iMac-de-STRATOS-2.local>
2025-06-13 01:34:46 -0600

    cambios 13 jun

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http'; // peticiones a la api
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { appsettings } from '../settings/appsettings'; //archivo de configurtacion de la ruta del servidor api
 import { ResponseUsuarios } from '../interfaces/ResponseUsuarios';
 import { User } from '../interfaces/User';
-import { Observable } from 'rxjs';
+import { Observable, throwError, catchError, BehaviorSubject , tap, map} from 'rxjs';
 
 @Injectable({
      providedIn: 'root'
@@ -14,12 +14,39 @@ export class UsuariosService {
      
     constructor() { }
 
+    listaU() : Observable<ResponseUsuarios>{
+          return  this.http.get<ResponseUsuarios>(`${this.baseUrl}usuarios/capacitadorlist`);
+    }
+
     listaUsers() : Observable<ResponseUsuarios>{
         return  this.http.get<ResponseUsuarios>(`${this.baseUrl}usuarios/listar`)
     }
 
     getUsers(id:number) : Observable<any>{
         return  this.http.get<any>(`${this.baseUrl}usuario/listar/`+id)
+    }
+
+    editUser(request:any):Observable<any>{
+        return this.http.put<any>(this.baseUrl+"usuario/update/", request).pipe(
+        tap( (userData) => {
+               
+            console.log(userData)
+              
+        }),
+        map((userData)=> userData),
+        catchError(this.handleError)
+        );
+    }
+
+    private handleError(error:HttpErrorResponse){
+        if(error.status===0){
+            console.error('Se ha producio un error ', error.error);
+        }
+        else{
+           console.error('Backend retornó el código de estado ', error);
+        }
+        
+        return throwError(()=> new Error('Algo falló. Por favor intente nuevamente.'));
     }
 
     
