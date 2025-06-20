fa09fa14eee2e2c1820d5aecc7b335ceeceda59e
Meme <cesar.merinopmc@gmail.com>
2025-06-19 23:51:55 -0600

    Cambios Merino 19062025

--- src/app/services/contacto.service.ts
+++ src/app/services/contacto.service.ts
@@ -1,9 +1,9 @@
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { appsettings } from '../settings/appsettings';
 import { ResponseContacto } from '../interfaces/ResponseContacto';
-import { Observable } from 'rxjs';
+import { Observable, throwError, catchError, BehaviorSubject , tap, map} from 'rxjs';
 
 
 @Injectable({
@@ -25,6 +25,28 @@ export class contactoService {
 
     getUsers(id:number) : Observable<any>{
         return  this.http.get<any>(`${this.baseUrl}contacto/listar/`+id)
+    }
+
+    register(credentials:ResponseContacto):Observable<any>{
+              return this.http.post<any>(this.baseUrl+"contacto/enviarContacto",credentials).pipe(
+              tap( (userData) => {
+                   
+                   console.log(userData)
+                  
+              }),
+              map((userData)=> userData.data.token),
+              catchError(this.handleError)
+              );
+         }
+
+    private handleError(error:HttpErrorResponse){
+              if(error.status===0){
+                   console.error('Se ha producio un error ', error.error);
+              }
+              else{
+               console.error('Backend retornó el código de estado ', error);
+              }
+              return throwError(()=> new Error('Algo falló. Por favor intente nuevamente.'));
     }
 
 
