55580e83431ea0031531f9ba5e997651a92ba15f
ruben <rubenrs.251@gmail.com>
2025-07-12 17:16:38 -0600

    12-07-2025 v2

--- src/app/services/inscripcion.service.ts
+++ src/app/services/inscripcion.service.ts
@@ -17,18 +17,41 @@ export class InscripcionService {
      
     constructor() { }
 
+
+    registrar(datos:ResponseInscripcion):Observable<any>{
+        return this.http.post<any>(this.baseUrl+"inscripcion",datos).pipe(
+        tap( (incripcionData) => {
+               
+            console.log(incripcionData)
+              
+        }),
+        map((incripcionData)=> incripcionData),
+        catchError(this.handleError)
+        );
+    }
+
     inscritosCurso(id:Number, idalumno:Number) : Observable<ResponseInscripcion>{
-            return  this.http.get<ResponseInscripcion>(`${this.baseUrl}inscripcion/inscritosCurso/`+{id}+`/`+{idalumno})
-        }
+        return  this.http.get<ResponseInscripcion>(`${this.baseUrl}inscripcion/inscritosCurso/`+{id}+`/`+{idalumno})
+    }
 
 
-   inscritosRecursos(id:Number, idalumno:Number) : Observable<ResponseInscripcion>{
-            return  this.http.get<ResponseInscripcion>(`${this.baseUrl}inscripcion/inscritosRecursos/`+{id}+`/`+{idalumno})
-        }
+    inscritosRecursos(id:Number, idalumno:Number) : Observable<ResponseInscripcion>{
+        return  this.http.get<ResponseInscripcion>(`${this.baseUrl}inscripcion/inscritosRecursos/`+{id}+`/`+{idalumno})
+    }
    
     cursosxalumno(id:Number) : Observable<ResponseCInscritos>{
-            return  this.http.get<ResponseCInscritos>(`${this.baseUrl}inscripcion/cursosxalumno/`+id)
-        }
+        return  this.http.get<ResponseCInscritos>(`${this.baseUrl}inscripcion/cursosxalumno/`+id)
+    }
+
+    private handleError(error:HttpErrorResponse){
+          if(error.status===0){
+               console.error('Se ha producio un error ', error.error);
+          }
+          else{
+           console.error('Backend retornó el código de estado ', error);
+          }
+          return throwError(()=> new Error('Algo falló. Por favor intente nuevamente.'));
+     }
     
 
 }
